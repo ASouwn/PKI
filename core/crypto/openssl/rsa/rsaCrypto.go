a309e3ac2f8f775c0c2681d4709bce7807e678d9
asouwn <asouwn@126.com>
2025-03-15 03:54:26 +0800

    generated the branch

--- core/crypto/openssl/rsa/rsaCrypto.go
+++ core/crypto/openssl/rsa/rsaCrypto.go
@@ -1,1 +1,36 @@
 package rsa
+
+import (
+	"encoding/pem"
+
+	"github.com/ASouwn/PKI/core/crypto"
+)
+
+type RSACrypto struct{}
+
+// Decrypt implements crypto.CryptoInterface.
+func (r *RSACrypto) Decrypt(ciphertext []byte, priBlock *pem.Block) ([]byte, error) {
+	panic("unimplemented")
+}
+
+// Encrypt implements crypto.CryptoInterface.
+func (r *RSACrypto) Encrypt(origin []byte, pubBlock *pem.Block) ([]byte, error) {
+	panic("unimplemented")
+}
+
+// GenerateKeyPair implements crypto.CryptoInterface.
+func (r *RSACrypto) GenerateKeyPair() (private *pem.Block, public *pem.Block, err error) {
+	panic("unimplemented")
+}
+
+// Sign implements crypto.CryptoInterface.
+func (r *RSACrypto) Sign(origin []byte, priBlock *pem.Block) ([]byte, error) {
+	panic("unimplemented")
+}
+
+// Verify implements crypto.CryptoInterface.
+func (r *RSACrypto) Verify(origin []byte, signature []byte, pubBlock *pem.Block) (bool, error) {
+	panic("unimplemented")
+}
+
+var _ crypto.CryptoInterface = (*RSACrypto)(nil)
