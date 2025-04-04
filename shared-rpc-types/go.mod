4026691cfc78a661a3de9d4632eb0a9b4add1d4d
asouwn <asouwn@126.com>
2025-04-04 19:43:59 +0800

    用于规范服务与发现机制的 RPC 类型定义 基于 Go 的 RPC 实现的服务与发现机制

---
+++ shared-rpc-types/go.mod
@@ -0,0 +1,3 @@
+module github.com/ASouwn/PKI/shared-rpc-types
+
+go 1.23.0
---
+++ shared-rpc-types/types.go
@@ -0,0 +1,32 @@
+package sharedrpctypes
+
+// This struct is used to register the server information
+// will get the server information from the client
+// network: tcp or udp, others the server provides
+// to use the addr: serverAddress+":"+port
+type ServerInfo struct {
+	Network       string
+	ServerAddress string
+	Port          string
+}
+
+// This struct is used to register the server information
+// function in regist server: func GetServer(args *ServerKey, reply *ServerInfo)
+// the reply is the ServerInfo struct
+type ServerKey struct {
+	ServerName string
+}
+
+// This struct is used to register the server information
+// function in regist server: func WriteServer(args *Server, reply *string)
+// the reply is the string "Server registered successfully"
+type Server struct {
+	ServerInfo ServerInfo
+	ServerKey  ServerKey
+}
+
+// 请通过RpcRegister.ServiceName来调用接口
+type RpcRegister interface {
+	WriteServer(args *Server, reply *string) error
+	GetServer(args *ServerKey, reply *ServerInfo) error
+}
