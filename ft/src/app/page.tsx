c05d18d35109adea0e22c5202fa78feb4ea18e57
ahsown <asouwn@126.com>
2025-04-17 10:37:47 +0800

    init frontend

---
+++ ft/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>客户端</div>
+      <div>RA</div>
+      <div>CA</div>
+      <div>服务注册发现中心</div>
+    </div>
+  );
+}
